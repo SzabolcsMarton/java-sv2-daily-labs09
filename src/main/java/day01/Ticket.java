90e06fac7104bc9f22854836e9f041e0dc074076
SzabolcsMarton <maszabolcs@gmail.com>
2021-12-07 13:18:35 +0100

    add the daily task

--- src/main/java/day01/Ticket.java
+++ src/main/java/day01/Ticket.java
@@ -1,4 +1,35 @@
 package day01;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Ticket {
+
+    private String nameOfBand;
+    private LocalDateTime startTime;
+    private int price;
+
+    public Ticket(String nameOfBand, LocalDateTime localDateTime, int price) {
+        this.nameOfBand = nameOfBand;
+        this.startTime = localDateTime;
+        this.price = price;
+    }
+
+    public LocalTime entryTime(){
+        return startTime.toLocalTime().minusHours(1);
+    }
+
+    public String getNameOfBand() {
+        return nameOfBand;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
 }
